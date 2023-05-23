5dce4ac6d4d9fc0fe2775ad7c01fdb1a91c42826
Samantha <samanthachin.dsp@gmail.com>
2023-05-23 15:24:41 -0700

    updated conference name, description and images

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,4 +1,38 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', async () => {
+
+    const url = 'http://localhost:8000/api/conferences/';
+
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        // Figure out what to do when the response is bad
+        console.error('Something went wrong, got an error in the response!')
+      } else {
+        const data = await response.json();
+
+        const conference = data.conferences[0];
+        const nameTag = document.querySelector('.card-title');
+        nameTag.innerHTML = conference.name;
+
+        const detailUrl = `http://localhost:8000${conference.href}`;
+        const detailResponse = await fetch(detailUrl);
+        if (detailResponse.ok) {
+          const details = await detailResponse.json();
+          const description = details.conference.description
+          const descriptionTag = document.querySelector('.card-text');
+          descriptionTag.innerHTML = description
+          const imageTag = document.querySelector('.card-img-top')
+          imageTag.src = details.conference.location.picture_url;
+        }
+
+      }
+    } catch (e) {
+      // Figure out what to do if an error is raised
+      console.log(e)
+    }
+
+  });
 
 
-});
+//
