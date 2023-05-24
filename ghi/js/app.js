4aace0b95c33f2730d4971c0a72c0becebcbcc5e
Samantha <samanthachin.dsp@gmail.com>
2023-05-23 19:34:13 -0700

    styled card template

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,3 +1,16 @@
+function createCard(name, description, pictureUrl) {
+    return `
+      <div class="card shadow">
+        <img src="${pictureUrl}" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">${name}</h5>
+          <p class="card-text">${description}</p>
+        </div>
+      </div>
+    `;
+  }
+
+
 window.addEventListener('DOMContentLoaded', async () => {
 
     const url = 'http://localhost:8000/api/conferences/';
@@ -10,29 +23,43 @@ window.addEventListener('DOMContentLoaded', async () => {
         console.error('Something went wrong, got an error in the response!')
       } else {
         const data = await response.json();
+        const conferenceRow = document.getElementById('conference-row');
 
-        const conference = data.conferences[0];
-        const nameTag = document.querySelector('.card-title');
-        nameTag.innerHTML = conference.name;
+        let columnCount = 0;
 
-        const detailUrl = `http://localhost:8000${conference.href}`;
-        const detailResponse = await fetch(detailUrl);
-        if (detailResponse.ok) {
-          const details = await detailResponse.json();
-          const description = details.conference.description
-          const descriptionTag = document.querySelector('.card-text');
-          descriptionTag.innerHTML = description
-          const imageTag = document.querySelector('.card-img-top')
-          imageTag.src = details.conference.location.picture_url;
+        for (let conference of data.conferences) {
+          const detailUrl = `http://localhost:8000${conference.href}`;
+          const detailResponse = await fetch(detailUrl);
+          if (detailResponse.ok) {
+            const details = await detailResponse.json();
+            const title = details.conference.name;
+            const description = details.conference.description;
+            const pictureUrl = details.conference.location.picture_url;
+            const html = createCard(title, description, pictureUrl);
+
+            if (columnCount % 3 === 0) {
+                const row = document.createElement('div');
+                row.classList.add('row', 'mb-3');
+                conferenceRow.appendChild(row);
+              }
+            // Create column and append card to it
+            const column = document.createElement('div');
+            column.classList.add('col');
+            column.innerHTML = html;
+
+            // Append column to the current row
+            const currentRow = conferenceRow.lastElementChild;
+            currentRow.appendChild(column);
+
+            columnCount++;
+          }
         }
 
       }
     } catch (e) {
       // Figure out what to do if an error is raised
       console.log(e)
+
     }
 
   });
-
-
-//
